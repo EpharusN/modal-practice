04c17308908cbfae8cd9ae45fa65acc9b83d7a2a
Epharus Ndumia <epharusndumia@gmail.com>
2024-10-18 20:52:53 +0300

    added event listeners to the buttons

--- script.js
+++ script.js
@@ -2,11 +2,30 @@
 
 const overlay = document.querySelector('.overlay');
 const modal = document.querySelector('.modal');
-const btnCloseOverlay = document.querySelector('.close-overlay');
-const btnShowModal = document.querySelectorAll('.show-modal');
-console.log(btnShowModal);
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+console.log(btnsShowModal);
 
-for (let i = 0; i < btnShowModal.length; i++)
-  btnShowModal[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-  });
+const showModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsShowModal.length; i++)
+  btnsShowModal[i].addEventListener('click', showModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
